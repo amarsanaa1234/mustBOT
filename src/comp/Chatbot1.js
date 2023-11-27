0d535170fe77b4943117f1c02409868ac5e5dcb7
Turmandakh.n <turmandakh.n@itc.gov.mn>
2023-11-28 01:39:44 +0800

    sada

---
+++ src/comp/Chatbot1.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { question, answer } from './data';
+import logo from '../img/logo.png'
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import SmartToyOutlinedIcon from '@mui/icons-material/SmartToyOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+const Chatbot = () => {
+  const [messages, setMessages] = useState([
+    ' Сайн уу! Та асуух зүйлээ доорх сонголтоос сонгоно уу?',
+  ]);
+  const [input, setInput] = useState('');
+
+  const getResponse = (message) => {
+    for (const key in answer) {
+      if (message.includes(key)) {
+        return answer[key];
+      }
+    }
+    return "Уучлаарай асуултыг ойлгосонгүй. Сонголтоос сонгоно уу!!!";
+  };
+
+  const clickme = (value) => {
+    console.log(value);
+    setMessages([...messages, `${value}`, ` ${getResponse(value)}`]);
+  }
+  
+
+  return (
+    <div className='container'>
+      <img src={logo} />
+      <div className="chat-container">
+        {messages.map((message, index) => (
+          <div className={message.startsWith(' ') ? 'chatRobot' : 'chatUser'}>
+            {message.startsWith(' ') ? <SmartToyOutlinedIcon fontSize="medium" />: <PersonOutlineOutlinedIcon fontSize="medium" />}
+            <div key={index} className={message.startsWith(' ') ? 'bot-message' : 'user-message'}>
+              {message}
+            </div>
+          </div>
+        ))}
+
+        <div className='questionBox'>
+          {question.map((option, index) => (
+              <button className='button' key={index} onClick={() => 
+                clickme(option.question)
+              }>
+                <div className='question'>
+                  {option.question}
+                </div>
+              </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chatbot;
